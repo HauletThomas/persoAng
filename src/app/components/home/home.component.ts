213e21a1bf2437c7755631fb842354b4a73d7209
Thomas <la207289@student.helha.be>
2025-01-12 14:03:04 +0100

    Update Home Component
    continuation of the homepage with the drawings + addition of the reset button + we can undo the drawing

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,7 +12,11 @@ export class HomeComponent implements AfterViewInit {
   private canvas!: HTMLCanvasElement;
   private ctx!: CanvasRenderingContext2D | null;
   private isDrawing = false;
-  private currentColor: string = 'green'; // Couleur initiale
+  private currentColor: string = 'green';
+  private isOverPalette = false;
+  private history: ImageData[] = [];
+
+
 
   colors: string[] = [
     'green', 'red', 'blue', 'yellow', 'orange', 'purple', 'pink', 'cyan', 'brown', 'black'
@@ -26,11 +30,11 @@ export class HomeComponent implements AfterViewInit {
 
   @HostListener('window:resize')
   resizeCanvas() {
-    const savedImage = this.ctx?.getImageData(0, 0, this.canvas.width, this.canvas.height); // Sauvegarde le dessin
+    const savedImage = this.ctx?.getImageData(0, 0, this.canvas.width, this.canvas.height);
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
     if (savedImage && this.ctx) {
-      this.ctx.putImageData(savedImage, 0, 0); // Restaure le dessin après redimensionnement
+      this.ctx.putImageData(savedImage, 0, 0);
     }
   }
 
@@ -38,6 +42,7 @@ export class HomeComponent implements AfterViewInit {
   startDrawing(event: MouseEvent) {
     if (this.ctx) {
       this.isDrawing = true;
+      this.saveHistory();
       this.ctx.beginPath();
       this.ctx.moveTo(event.offsetX, event.offsetY);
     }
@@ -45,10 +50,10 @@ export class HomeComponent implements AfterViewInit {
 
   @HostListener('mousemove', ['$event'])
   draw(event: MouseEvent) {
-    if (this.isDrawing && this.ctx) {
-      this.ctx.strokeStyle = this.currentColor; // Utilise la couleur actuelle
-      this.ctx.lineWidth = 5; // Épaisseur de la ligne
-      this.ctx.lineCap = 'round'; // Fini arrondi
+    if (this.isDrawing && this.ctx && !this.isMouseOverPalette(event)) {
+      this.ctx.strokeStyle = this.currentColor;
+      this.ctx.lineWidth = 5;
+      this.ctx.lineCap = 'round';
       this.ctx.lineTo(event.offsetX, event.offsetY);
       this.ctx.stroke();
     }
@@ -62,10 +67,12 @@ export class HomeComponent implements AfterViewInit {
     }
   }
 
-  // @HostListener('mouseleave')
-  // cancelDrawing() {
-  //   this.isDrawing = false;
-  // }
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.ctrlKey && event.key === 'z') {
+      this.undo(); // Appelle la fonction Undo avec Ctrl+Z
+    }
+  }
 
   saveCanvas() {
     if (this.canvas) {
@@ -79,5 +86,48 @@ export class HomeComponent implements AfterViewInit {
   // Change la couleur de dessin
   changeColor(color: string) {
     this.currentColor = color;
+  }
+
+  private isMouseOverPalette(event: MouseEvent): boolean {
+    const palette = document.getElementById('colorPalette');
+    if (palette) {
+      const rect = palette.getBoundingClientRect();
+      return (
+        event.clientX >= rect.left &&
+        event.clientX <= rect.right &&
+        event.clientY >= rect.top &&
+        event.clientY <= rect.bottom
+      );
+    }
+    return false;
+  }
+  onPaletteEnter() {
+    this.isOverPalette = true;
+    this.isDrawing = false;
+  }
+
+  onPaletteLeave() {
+    this.isOverPalette = false;
+  }
+  resetCanvas() {
+    if (this.ctx) {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.history = [];
+    }
+  }
+
+  undo() {
+    if (this.ctx && this.history.length > 0) {
+      const lastState = this.history.pop();
+      if (lastState) {
+        this.ctx.putImageData(lastState, 0, 0);
+      }
+    }
+  }
+  private saveHistory() {
+    if (this.ctx) {
+      const snapshot = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+      this.history.push(snapshot);
+    }
   }
 }
